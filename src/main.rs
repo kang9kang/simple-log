ad0d8ba92cfe9adb1343db723de40139e5490eec
kang9kang <13589130504@163.com>
2025-12-29 15:47:45 +0800

    添加日志功能，创建 log_time 函数以记录当前时间到文件

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 extern crate chrono;
+
+use std::io::prelude::*;
+use std::fs::File;
+use std::io;
 use chrono::*;
 
-fn main() {
+fn log_time(filename: &'static str) -> io::Result<()> {
     let local: DateTime<Local> = Local::now();
-    println!("{}", local);
+    let formatted_time = local.format("%Y").to_string();
+    let bytes = formatted_time.as_bytes();
+    let mut f = File::create(filename)?;
+    f.write_all(bytes)?;
+    Ok(())
+}
+
+fn main() {
+    match log_time("log.txt") {
+        Ok(..) => println!("File created!"),
+        Err(..) => println!("Error: could not create file.")
+    }
 }
